a7741986a2d6b359ef1f60c5b3748cd3221ed3fb
little_xuuu <little_xuuu@163.com>
2021-06-24 22:26:54 +0800

    功能1. 给minisign添加一个属性itemname。当画的时候设置这个属性

--- modmain.lua
+++ modmain.lua
@@ -33,21 +33,18 @@ _G.TheInput:AddKeyUpHandler(117, showInfo)
 
 -- 给minisign添加一个属性itemname。当画的时候设置这个属性
 local function MinisignAddItemname(inst)
+	inst.itemname = nil
 	local old_draw = inst.components.drawable.ondrawnfn
 	inst.components.drawable:SetOnDrawnFn(function(inst, image, src, atlas, bgimage, bgatlas)
 		--print("[master chest] ondraw")
-		local x, y, z = inst.Transform:GetWorldPosition()
-		--print(x,y,z)
+		--获取src的prefab名称
+		local tag = src.entity:GetDebugString()
+		-- print("[showInfo] " .. tag)
+		local index1 = string.find(tag, "Prefab:",0)
+		local index2 = string.find(tag, "\n", index1)
+		inst.itemname = string.sub(tag,index1+8,index2-1)
 		old_draw(inst, image, src, atlas, bgimage, bgatlas)
-		findchest(x,y,z)
 	end)
-	local old_onload = inst.OnLoad
-	inst.OnLoad = function(inst, data)
-		--print("[master chest] onload")
-		old_onload(inst, data)
-		local x, y, z = inst.Transform:GetWorldPosition()
-		findchest(x,y,z)
-	end
 end
 AddPrefabPostInit("minisign", MinisignAddItemname)
 
