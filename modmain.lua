7999335daf0d20e23918cd9c1ffc760da760f3e3
little_xuuu <little_xuuu@163.com>
2021-06-24 22:41:42 +0800

    功能1. 添加箱子打开的部分

--- modmain.lua
+++ modmain.lua
@@ -29,22 +29,34 @@ local function showInfo()
 	end
 
 end
-_G.TheInput:AddKeyUpHandler(117, showInfo)
+_G.TheInput:AddKeyUpHandler(118, showInfo)
 
--- 给minisign添加一个属性itemname。当画的时候设置这个属性
+-- 给minisign添加一个属性 itemname 。当画的时候设置这个属性
 local function MinisignAddItemname(inst)
 	inst.itemname = nil
 	local old_draw = inst.components.drawable.ondrawnfn
-	inst.components.drawable:SetOnDrawnFn(function(inst, image, src, atlas, bgimage, bgatlas)
+	inst.components.drawable:SetOnDrawnFn(function(inst2, image, src, atlas, bgimage, bgatlas)
 		--print("[master chest] ondraw")
 		--获取src的prefab名称
 		local tag = src.entity:GetDebugString()
 		-- print("[showInfo] " .. tag)
 		local index1 = string.find(tag, "Prefab:",0)
 		local index2 = string.find(tag, "\n", index1)
-		inst.itemname = string.sub(tag,index1+8,index2-1)
-		old_draw(inst, image, src, atlas, bgimage, bgatlas)
+		inst2.itemname = string.sub(tag,index1+8,index2-1)
+		old_draw(inst2, image, src, atlas, bgimage, bgatlas)
 	end)
 end
 AddPrefabPostInit("minisign", MinisignAddItemname)
 
+-- 给箱子添加 LXautocollectitems 这个component,并且箱子打开时设置要收集的items
+local function ContainerAddACI(inst) -- ACI == LXautocollectitems
+	if inst and inst.components and inst.components.container then
+		inst:AddComponent("lxautocollectitems")
+		local old_open = inst.components.container.onopenfn
+		inst.components.container.onopenfn = function(inst2)
+			inst2.components.lxautocollectitems:SetItems()
+			old_open(inst2)
+		end
+	end
+end
+AddPrefabPostInit("treasurechest", ContainerAddACI)
