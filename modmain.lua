97c2cd5de381b9d4d2b3c236fcc2e903af148d2a
little_xuuu <little_xuuu@163.com>
2021-06-25 11:05:26 +0800

    功能1 完成。最终需要收集的物品保存在lxautocolectitems中

--- modmain.lua
+++ modmain.lua
@@ -17,36 +17,112 @@ local IsServer = TheNet:GetIsServer() or TheNet:IsDedicated()
 local function showInfo()
 	local target = _G.TheInput:GetWorldEntityUnderMouse()
 	if target ~= nil then
-		local tag = target.entity:GetDebugString()
-		-- print("[showInfo] " .. tag)
-		local index1 = string.find(tag, "Prefab:",0)
-		local index2 = string.find(tag, "\n", index1)
-		print("[showInfo] Prefab weizhi:" .. index1)
-		print("[showInfo] \\n weizhi:" .. index2)
-		local s = string.sub(tag,index1+8,index2-1)
-		print("[showInfo] " .. s)
-		print(s .. " len = " .. string.len(s))
+		if target.prefab == "minisign" or target.prefab == "minisign_drawn" then
+			if target._imagename then
+				print("[showInfo] minisign._imagename:" .. target._imagename:value())
+			else
+				print("[showInfo] minisign.itemname == nil")
+			end
+		end
+		if target.prefab == "treasurechest" then
+			print("[showInfo] treasurechest.items : start")
+			if target and target.components.lxautocollectitems then
+				for key, value in pairs(target.components.lxautocollectitems.items) do
+					print(value)
+				end
+			end
+			print("[showInfo] treasurechest.items : end")
+		end
 	end
 
 end
 _G.TheInput:AddKeyUpHandler(118, showInfo)
 
--- 给minisign添加一个属性 itemname 。当画的时候设置这个属性
+-- 使用minisign的属性_imagename（所以这里就不需要修改了）
+--[[
 local function MinisignAddItemname(inst)
 	inst.itemname = nil
-	local old_draw = inst.components.drawable.ondrawnfn
-	inst.components.drawable:SetOnDrawnFn(function(inst2, image, src, atlas, bgimage, bgatlas)
-		--print("[master chest] ondraw")
-		--获取src的prefab名称
-		local tag = src.entity:GetDebugString()
-		-- print("[showInfo] " .. tag)
-		local index1 = string.find(tag, "Prefab:",0)
-		local index2 = string.find(tag, "\n", index1)
-		inst2.itemname = string.sub(tag,index1+8,index2-1)
-		old_draw(inst2, image, src, atlas, bgimage, bgatlas)
-	end)
+	if inst and inst.components and inst.components.drawable then
+		--print("change ondrawnfn")
+		local old_draw = inst.components.drawable.ondrawnfn
+		inst.components.drawable:SetOnDrawnFn(function(inst2, image, src, atlas, bgimage, bgatlas)
+			--print("[master chest] ondraw")
+			if src ~= nil then
+				--获取src的prefab名称
+				inst2.itemname = src.prefab
+			end
+			old_draw(inst2, image, src, atlas, bgimage, bgatlas)
+		end)
+	end
+
+	--修改dig_up
+	if inst and inst.components and inst.components.workable then
+		--print("change dig_up")
+		--local old_onfinish = inst.components.workable.onfinish
+		inst.components.workable:SetOnFinishCallback(function(inst2)
+			local image = inst2.components.drawable:GetImage()
+			if image ~= nil then
+				local item = inst2.components.lootdropper:SpawnLootPrefab("minisign_drawn", nil, inst2.linked_skinname_drawn, inst2.skin_id )
+				item.components.drawable:OnDrawn(image, nil, inst2.components.drawable:GetAtlas(), inst2.components.drawable:GetBGImage(), inst2.components.drawable:GetBGAtlas())
+				item._imagename:set(inst2._imagename:value())
+				if inst2.itemname then
+					--print("[dig_ip] inst.itemname:" .. inst2.itemname)
+					item.itemname = inst2.itemname
+				end
+			else
+				inst2.components.lootdropper:SpawnLootPrefab("minisign_item", nil, inst2.linked_skinname, inst2.skin_id )
+			end
+			inst2:Remove()
+		end)
+	end
+
+	--修改ondeploy
+	if inst and inst.components and inst.components.deployable then
+		--print("change ondeploy")
+		inst.components.deployable.ondeploy = function(inst2, pt)
+			local ent = _G.SpawnPrefab("minisign", inst2.linked_skinname, inst2.skin_id )
+
+			if inst2.components.stackable ~= nil then
+				inst2.components.stackable:Get():Remove()
+			else
+				ent.components.drawable:OnDrawn(inst2.components.drawable:GetImage(), nil, inst2.components.drawable:GetAtlas(), inst2.components.drawable:GetBGImage(), inst2.components.drawable:GetBGAtlas())
+				ent._imagename:set(inst2._imagename:value())
+				if inst2.itemname then
+					--print("[ondeply] inst.itemname:" .. inst2.itemname)
+					ent.itemname = inst2.itemname
+				end
+				inst2:Remove()
+			end
+
+			ent.Transform:SetPosition(pt:Get())
+			ent.SoundEmitter:PlaySound("dontstarve/common/sign_craft")
+		end
+	end
+
+	--保存和加载
+	if inst.OnSave ~= nil then
+		local old_OnSave = inst.OnSave
+		inst.OnSave = function(inst2, data)
+			data.itemname = inst2.itemname
+			old_OnSave(inst2, data)
+		end
+	end
+	if inst.OnLoad ~= nil then
+		local old_OnLoad = inst.OnLoad
+		--print("old_OnLoad = " .. old_OnLoad)
+		inst.OnLoad = function(inst2, data)
+			if data.itemname then
+				inst2.itemname = data.itemname
+			end
+			old_OnLoad(inst2, data)
+		end
+	end
+
 end
 AddPrefabPostInit("minisign", MinisignAddItemname)
+AddPrefabPostInit("minisign_item", MinisignAddItemname)
+AddPrefabPostInit("minisign_drawn", MinisignAddItemname)
+]]--
 
 -- 给箱子添加 LXautocollectitems 这个component,并且箱子打开时设置要收集的items
 local function ContainerAddACI(inst) -- ACI == LXautocollectitems
--- scripts/components/lxautocollectitems.lua
+++ scripts/components/lxautocollectitems.lua
@@ -1,24 +1,14 @@
--- 让container自动收集附近物品的component。
--- 功能1：设置要收集的物品。
--- 			通过打开container来设置
--- 			设置的内容来自附近的小木牌（需要给minisign加一个属性itemname，当写的时候赋值）
---			根据设置选项中的内容来决定是只收集一种还是好多种。
--- 功能2：收集物品
---			以下几种情况时触发收集物品功能
---			1. 玩家丢弃（pick up）物品时
---			2. 生物死亡掉落物品时
 local LXautocollectitems = Class(function(self, inst)
 	self.inst = inst
 	self.iscollect = false -- 判断是否开始收集
 	self.items = {}
-	self.setitemsfn = nil
 end,
 nil,
 nil)
 
-local _G = GLOBAL
-local minisigndist = GetModConfigData("minisign_dist")
-local iscollectone = GetModConfigData("iscollectone")
+local modname = KnownModIndex:GetModActualName(" 成熟的箱子2 - Local")
+local minisigndist = GetModConfigData("minisign_dist", modname)
+local iscollectone = GetModConfigData("iscollectone", modname)
 
 --[[
 local function getMinisign(inst){
@@ -36,27 +26,45 @@ local function getMinisign(inst){
 
 -- 功能1：设置要收集的物品。
 function LXautocollectitems:SetItems()
-	if self.inst and self.inst.container then 
+	print("[LXautocollectitems]SetItems enter")
+	if self.inst and self.inst.components and self.inst.components.container then 
 		self.items = {} -- 清空当前的设置
 		-- 获取周围的小木牌
 		local x, y, z = self.inst.Transform:GetWorldPosition()
-		local ents = _G.TheSim:FindEntities(x, y, z, minisigndist, { "sign", "drawable" }, { "INLIMBO"}) --包括所有木牌
+		local ents = TheSim:FindEntities(x, y, z, minisigndist, { "sign" }, { "INLIMBO"}) --包括所有木牌
+		print("#minisign = " .. #ents)
 		-- 筛选画好的,并填入items
 		for i, v in ipairs(ents) do
-			if v and v.prefab == "minisign" and v.components and v.components.drawable and v.components.drawable.candraw == false then
-				table.insert(self.items, v.itemname)
+			if v and v.prefab == "minisign" and v.components and v.components.drawable and v.components.drawable:GetImage() ~= nil then
+				table.insert(self.items, v._imagename:value())
 				if iscollectone == 1 then 
 					break
 				end
 			end
 		end
+		for key, value in pairs(self.items) do
+			print("" .. key .. " " .. value)
+		end
 	end
+	print("[LXautocollectitems]SetItems exit")
 end
 
 -- 功能2：收集物品
 
 
+-- 保存和加载
+function LXautocollectitems:OnSave()
+	local data = {
+		iscollect = self.iscollect,
+		items = self.items
+	}
+	return data
+end
 
+function LXautocollectitems:OnLoad(data)
+	self.iscollect = data.iscollect
+	self.items = data.items
+end
 
 
 return LXautocollectitems
